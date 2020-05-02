02f4ba1654ae92d04574c282fcd38899588db3a0
al375877 <57473899+al375877@users.noreply.github.com>
2020-05-02 15:55:57 +0200

    Peticiones2

--- src/main/java/es/uji/ei1027/Mayorescasa/controller/UserController.java
+++ src/main/java/es/uji/ei1027/Mayorescasa/controller/UserController.java
@@ -2,19 +2,17 @@ package es.uji.ei1027.Mayorescasa.controller;
 
 import javax.servlet.http.HttpSession;
 
+import es.uji.ei1027.Mayorescasa.dao.UserDao;
+import es.uji.ei1027.Mayorescasa.model.UserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import es.uji.ei1027.Mayorescasa.dao.UserDao;
-import es.uji.ei1027.Mayorescasa.model.UserDetails;
-
 @Controller
-@RequestMapping("user")
+@RequestMapping("/user")
 public class UserController {
     private UserDao userDao;
-
     @Autowired
     public void setSociDao(UserDao userDao) {
         this.userDao = userDao;
@@ -22,23 +20,12 @@ public class UserController {
 
     @RequestMapping("/list")
     public String listSocis(HttpSession session, Model model) {
-        if (session.getAttribute("user") == null){
-            UserDetails user =new UserDetails();
-            model.addAttribute("user", user);
-            session.setAttribute("nextUrl", "beneficiario/index");
-            session.setAttribute("autorizado",user.getAutorizado());
+        if (session.getAttribute("user") == null)
+        {
+            model.addAttribute("user", new UserDetails());
             return "login";
-        }else {
-            UserDetails user = (UserDetails) session.getAttribute("user");
-            session.setAttribute("autorizado",user.getAutorizado());
         }
         model.addAttribute("users", userDao.listAllUsers());
         return "user/list";
     }
-
-    @RequestMapping("/index")
-    public String index(HttpSession session, Model model) {
-        return "user/index";
-    }
 }
-
