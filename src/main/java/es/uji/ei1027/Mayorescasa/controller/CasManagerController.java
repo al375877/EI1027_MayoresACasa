0e643d5cf0c080745ca72ba61847b9ae075ec001
al375877 <57473899+al375877@users.noreply.github.com>
2020-07-03 11:10:20 +0200

    2Conv_Arreglos24

--- src/main/java/es/uji/ei1027/Mayorescasa/controller/CasManagerController.java
+++ src/main/java/es/uji/ei1027/Mayorescasa/controller/CasManagerController.java
@@ -1,8 +1,11 @@
 package es.uji.ei1027.Mayorescasa.controller;
 
+import es.uji.ei1027.Mayorescasa.dao.UsuarioDao;
 import es.uji.ei1027.Mayorescasa.model.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
@@ -10,6 +13,15 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/casmanager")
 public class CasManagerController {
+
+    @Autowired
+    private UsuarioDao usuarioDao;
+
+    @Autowired
+    public void setUsuarioDao(UsuarioDao usuarioDao) {
+
+        this.usuarioDao=usuarioDao;
+    }
 
     @RequestMapping("/index")
     public String index(HttpSession session) {
@@ -21,6 +33,20 @@ public class CasManagerController {
             }
         } catch (Exception ex){
             System.out.println("Acceso prohibido");
+            return "error/error";
+        }
+        return "error/error";
+    }
+
+    @RequestMapping("/unilist/{usuario}")
+    public String unilistEmpresasPropio(HttpSession session, Model model, @PathVariable String usuario) {
+        Usuario user= (Usuario) session.getAttribute("user");
+        try{
+            if(user.getTipoUsuario().equals("casManager") || user.getTipoUsuario().equals("casCommitee") || user.getTipoUsuario().equals("casVolunteer")) {
+                model.addAttribute("user", usuarioDao.getUsuario(usuario));
+                return "casmanager/unilist";
+            }
+        } catch (Exception e) {
             return "error/error";
         }
         return "error/error";
